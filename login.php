42de3d041bcf15e506cfc74c1d74cf03d81439b3
DanielIC <43577539+danielindracahyadi@users.noreply.github.com>
2019-03-07 14:24:14 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" type="text/css" href="login.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 	<style>
 		body {
 			background-color: #10171e;
@@ -80,6 +81,7 @@
 					} else{
 						//alert(data);
 						//location.reload()
+						 document.location.href = 'layout_disparity.php';
 					}
 				}
 			});
@@ -98,13 +100,23 @@
 						alert("Email or Password not exist.");
 					} else{
 						//alert(data);
-						//location.reload()
+						location.reload()
 					}
 				}
 			});
 			return false;
 		});
 	});
+
+	function funcLogOut(){
+			$.ajax({
+				url:"action_logout.php",
+				type:"GET",
+				success:function(data) {
+					location.reload();
+				}
+			});
+		}
 
 	function funcRegister(){
 		$.ajax({
