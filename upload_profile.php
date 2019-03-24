d82573508b417e2d044ff62d69c27077118d386c
antoniusfilian <45317076+antoniusfilian@users.noreply.github.com>
2019-03-24 21:57:23 +0700

    Add files via upload

--- upload_profile.php
+++ upload_profile.php
@@ -23,28 +23,37 @@
 	$stmtupdate = "UPDATE usr SET profilePicture = '$target_file' where tag = '$tagnya'";
 	$result = mysqli_query($con, $stmtupdate);
 
-
-	$uploadOk = 0;
-	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-	// Check if image file is a actual image or fake image
-	if(isset($_POST["submit"])) {
-	    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-	    if($check !== false) {
-	        echo "File is an image - " . $check["mime"] . ".";
-	        $uploadOk = 1;
-	    } else {
-	        echo "File is not an image.";
-	        $uploadOk = 0;
-	    }
+	if($_FILES["fileToUpload"]["tmp_name"] == null)
+	{
+		header("Location: index.php");
+		$message = "wrong answer";
+		echo "<script type='text/javascript'>alert('$message');</script>";
 	}
-	if ($uploadOk == 0) {
-	    echo "Sorry, your file was not uploaded.";
-	// if everything is ok, try to upload file
-	} else {
-	    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-	        header("Location: index.php");
-	    } else {
-	        echo "Sorry, there was an error uploading your file.";
-	    }
+	else
+	{
+		$uploadOk = 0;
+		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		// Check if image file is a actual image or fake image
+		if(isset($_POST["submit"])) {
+		    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		    if($check !== false) {
+		        echo "File is an image - " . $check["mime"] . ".";
+		        $uploadOk = 1;
+		    } else {
+		        echo "File is not an image.";
+		        $uploadOk = 0;
+		    }
+		}
+		if ($uploadOk == 0) {
+		    echo "Sorry, your file was not uploaded.";
+		// if everything is ok, try to upload file
+		} else {
+		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		        header("Location: index.php");
+		    } else {
+		        echo "Sorry, there was an error uploading your file.";
+		    }
+		}
 	}
+	
 ?>
